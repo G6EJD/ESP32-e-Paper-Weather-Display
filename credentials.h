e952d415edf632b1c8c599514694cd392e1b2e5e
G6EJD <d.l.bird@sky.com>
2018-04-12 20:47:04 +0100

    Add files via upload

---
+++ credentials.h
@@ -0,0 +1,14 @@
+// Change to your WiFi credentials
+const char* ssid1     = "your_SSID_here";
+const char* password1 = "your_PASSWORD_here";
+const char* ssid2     = "ssid-2";  // e.g. of a network extender
+const char* password2 = "password-2";
+
+// Use your own API key by signing up for a free developer account at http://www.wunderground.com/weather/api/
+String apikey     = "YOUR API KEY here"; // e.g. '2dbfa31748f4ab1'      // See: http://www.wunderground.com/weather/api/d/docs (change for your KEY)
+
+// Set your location according to WU locations
+const char* host    = "api.wunderground.com";
+String      City    = "Melksham";             // Your home city
+String      Country = "UK";                   // Your country  
+
