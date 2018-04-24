a0e3502c6b568ade405a1d28935f8523425e45a4
G6EJD <d.l.bird@sky.com>
2018-04-24 12:45:49 +0000

    Update credentials.h
    Improved guidance

--- credentials.h
+++ credentials.h
@@ -7,8 +7,9 @@ const char* password2 = "password-2";
 // Use your own API key by signing up for a free developer account at http://www.wunderground.com/weather/api/
 String apikey     = "YOUR API KEY here"; // e.g. '2dbfa31748f4ab1'      // See: http://www.wunderground.com/weather/api/d/docs (change for your KEY)
 
-// Set your location according to WU locations
-const char* host    = "api.wunderground.com";
-String      City    = "Melksham";             // Your home city
-String      Country = "UK";                   // Your country  
-
+// Set your location according to WU locations// https://www.wunderground.com/weather-by-country.asp
+// Click on your Country name, then choose a City near to you
+// If in the US choose a State, then City
+const char* host    = "api.wunderground.com";
+String      City    = "Melksham";             // Your home city
+String      Country = "UK";                   // Your country  
