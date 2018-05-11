507569600a8c848e47f04f51f785ae5f824a0029
G6EJD <d.l.bird@sky.com>
2018-05-11 21:59:16 +0000

    Update ESP32_WU_Current_Forecast_42_epaper_v2.ino

--- ESP32_WU_Current_Forecast_42_epaper_v2.ino
+++ ESP32_WU_Current_Forecast_42_epaper_v2.ino
@@ -103,7 +103,7 @@ void setup() {
   lastConnectionTime = millis();
   bool Received_Forecast_OK = obtain_forecast("conditions","forecast","astronomy");
   // Now only refresh the screen if all the data was received OK, otherwise wait until the next timed check
-  if (Received_Forecast_OK || (WiFi.status() == WL_CONNECTED)) { 
+  if (Received_Forecast_OK && (WiFi.status() == WL_CONNECTED)) { 
     //Received data OK at this point so turn off the WiFi to save power
     WiFi.mode(WIFI_OFF);
     gfx.init();
