bec988ac5a0dc5d1de9b4017116807f46d695f81
G6EJD <d.l.bird@sky.com>
2019-02-13 15:45:09 +0000

    Update ESP32_OWM_Current_Forecast_75_epaper_v8_GxEPD.ino

--- ESP32_OWM_Current_Forecast_75_epaper_v8_GxEPD.ino
+++ ESP32_OWM_Current_Forecast_75_epaper_v8_GxEPD.ino
@@ -1,4 +1,4 @@
-/* ESP32 Weather Display using an EPD 4.2" Display, obtains data from Open Weather Map, decodes it and then displays it.
+/* ESP32 Weather Display using an EPD 7.5" Display, obtains data from Open Weather Map, decodes it and then displays it.
   ####################################################################################################################################
   This software, the ideas and concepts is Copyright (c) David Bird 2018. All rights to this software are reserved.
 
