7eec5fd4448317c2207d490bc9437d83abb436ec
Andreas Lawitzky <a.lawitzky@tum.de>
2019-05-30 11:27:47 +0200

    Julian Date in common functions

--- src/common_functions.cpp
+++ src/common_functions.cpp
@@ -8,3 +8,17 @@ float hPa_to_inHg(float value_hPa)
 {
   return 0.02953 * value_hPa;
 }
+
+int JulianDate(int d, int m, int y) {
+  int mm, yy, k1, k2, k3, j;
+  yy = y - (int)((12 - m) / 10);
+  mm = m + 9;
+  if (mm >= 12) mm = mm - 12;
+  k1 = (int)(365.25 * (yy + 4712));
+  k2 = (int)(30.6001 * mm + 0.5);
+  k3 = (int)((int)((yy / 100) + 49) * 0.75) - 38;
+  // 'j' for dates in Julian calendar:
+  j = k1 + k2 + d + 59 + 1;
+  if (j > 2299160) j = j - k3; // 'j' is the Julian date at 12h UT (Universal Time) For Gregorian calendar:
+  return j;
+}
