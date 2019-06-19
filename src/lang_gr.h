59e24a1178d1067477dddc451aebd8ecd0c8647f
G6EJD <d.l.bird@sky.com>
2019-06-19 10:26:10 +0100

    Add files via upload

---
+++ src/lang_gr.h
@@ -0,0 +1,89 @@
+//Temperatur - Luftfeuchtigkeit - Vorschau
+const String TXT_FORECAST_VALUES  = "3 Tages Vorhersage";
+const String TXT_CONDITIONS       = "Verhaeltnisse";
+const String TXT_DAYS             = "(Tage)";
+const String TXT_TEMPERATURES     = "Temperatur";
+const String TXT_TEMPERATURE_C    = "Temperatur(*C)";
+const String TXT_TEMPERATURE_F    = "Temperatur(*F)";
+const String TXT_HUMIDITY_PERCENT = "Luftfeuchtigkeit(%)";
+
+//Luftdruck
+const String TXT_PRESSURE         = "Luftdruck";
+const String TXT_PRESSURE_HPA     = "Luftdruck(hpa)";
+const String TXT_PRESSURE_IN      = "Luftdrucksure(in)";
+const String TXT_PRESSURE_STEADY  = "bestaendig";
+const String TXT_PRESSURE_RISING  = "steigend";
+const String TXT_PRESSURE_FALLING = "fallend";
+
+//Niederschlag / Schneefall
+const String TXT_RAINFALL_MM = "Niederschlag(mm)";
+const String TXT_RAINFALL_IN = "Niederschlag(in)";
+const String TXT_SNOWFALL_MM = "Schneefall(mm)";
+const String TXT_SNOWFALL_IN = "Schneefall(in)";
+const String TXT_PRECIPITATION_SOON = "Regen/Schnee";
+const String TXT_RAIN               = " Regen";
+const String TXT_SNOW               = " Schnee";
+const String TXT_EQUAL_RAIN         = " = Regen";
+const String TXT_EQUAL_SNOW         = " = Schnee";
+
+//Sonne
+const String TXT_SUNRISE  = "Aufgang";
+const String TXT_SUNSET   = "Unterg.";
+
+//Mond
+const String TXT_MOON_NEW             = "Neu";
+const String TXT_MOON_WAXING_CRESCENT = "Wachsender Halbmond";
+const String TXT_MOON_FIRST_QUARTER   = "Erstes Viertel";
+const String TXT_MOON_WAXING_GIBBOUS  = "Wachsender Dreiviertelmond";
+const String TXT_MOON_FULL            = "Vollmond";
+const String TXT_MOON_WANING_GIBBOUS  = "Abnehmender Dreiviertelmond";
+const String TXT_MOON_THIRD_QUARTER   = "Drittels Viertel";
+const String TXT_MOON_WANING_CRESCENT = "Abnehmender Halbmond";
+
+//Energie / WLAN
+const String TXT_POWER  = "AKKU";
+const String TXT_WIFI   = "WLAN";
+const char* TXT_UPDATED = "Aktualisiert:";
+
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "Wind Geschw./Richtung";
+const String TXT_N   = "N";
+const String TXT_NNE = "NNO";
+const String TXT_NE  = "NO";
+const String TXT_ENE = "ONO";
+const String TXT_E   = "O";
+const String TXT_ESE = "OSO";
+const String TXT_SE  = "SO";
+const String TXT_SSE = "SSO";
+const String TXT_S   = "S";
+const String TXT_SSW = "SSW";
+const String TXT_SW  = "SW";
+const String TXT_WSW = "WSW";
+const String TXT_W   = "W";
+const String TXT_WNW = "WNW";
+const String TXT_NW  = "NW";
+const String TXT_NNW = "NNW";
+
+//Wochentag
+const char* TXT_MONDAY    = "Mo";
+const char* TXT_TUESDAY   = "Di";
+const char* TXT_WEDNESDAY = "Mi";
+const char* TXT_THURSDAY  = "Do";
+const char* TXT_FRIDAY    = "Fr";
+const char* TXT_SATURDAY  = "Sa";
+const char* TXT_SUNDAY    = "So";
+
+//Monat
+const char* TXT_JANUARY   = "Jan";
+const char* TXT_FEBRUARY  = "Feb";
+const char* TXT_MARCH     = "Mar";
+const char* TXT_APRIL     = "Apr";
+const char* TXT_MAY       = "Mai";
+const char* TXT_JUNE      = "Jun";
+const char* TXT_JULY      = "Jul";
+const char* TXT_AUGUST    = "Aug";
+const char* TXT_SEPTEMBER = "Sep";
+const char* TXT_OCTOBER   = "Okt";
+const char* TXT_NOVEMBER  = "Nov";
+const char* TXT_DECEMBER  = "Dez";
