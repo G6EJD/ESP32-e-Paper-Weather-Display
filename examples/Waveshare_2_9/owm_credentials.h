a09862641a7545e187a88f7d074b6aac54b30292
G6EJD <d.l.bird@sky.com>
2019-06-24 18:28:16 +0100

    Update owm_credentials.h

--- examples/Waveshare_2_9/owm_credentials.h
+++ examples/Waveshare_2_9/owm_credentials.h
@@ -1,8 +1,6 @@
 // Change to your WiFi credentials
 const char* ssid1     = "your-SSID";
 const char* password1 = "your-PASSWORD";
-const char* ssid2     = "ssid-2";  // e.g. of a network extender
-const char* password2 = "password-2";
 
 // Use your own API key by signing up for a free developer account at https://openweathermap.org/
 String apikey       = "your_OWM_API_key";                   // See: https://openweathermap.org/
@@ -11,7 +9,7 @@ const char server[] = "api.openweathermap.org";
 //http://api.openweathermap.org/data/2.5/weather?q=Melksham,UK&APPID=your_OWM_API_key&mode=json&units=metric&cnt=1
 //Set your location according to OWM locations
 String City          = "MELKSHAM";                      // Your home city See: http://bulk.openweathermap.org/sample/
-String Country       = "UK";                            // Your country  
+String Country       = "UK";                            // Your country, find your (nearest) city and the country code is displayed  
 String Language      = "EN";                            // NOTE: Only the weather description (not used) is translated by OWM
                                                         // Arabic (AR) Czech (CZ) English (EN) Greek (EL) Persian(Farsi) (FA) Galician (GL) Hungarian (HU) Japanese (JA)
                                                         // Korean (KR) Latvian (LA) Lithuanian (LT) Macedonian (MK) Slovak (SK) Slovenian (SL) Vietnamese (VI)
