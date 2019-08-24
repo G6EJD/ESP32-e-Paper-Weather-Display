10e7e25e114dc1acd19145ad3579442db8840376
G6EJD <d.l.bird@sky.com>
2019-08-24 15:45:57 +0100

    Update Waveshare_4_2.ino

--- examples/Waveshare_4_2/Waveshare_4_2.ino
+++ examples/Waveshare_4_2/Waveshare_4_2.ino
@@ -37,7 +37,7 @@ enum alignment {LEFT, RIGHT, CENTER};
 
 // Connections for e.g. LOLIN D32
 static const uint8_t EPD_BUSY = 4;  // to EPD BUSY
-static const uint8_t EPD_SS   = 5;  // to EPD CS
+static const uint8_t EPD_CS   = 5;  // to EPD CS
 static const uint8_t EPD_RST  = 16; // to EPD RST
 static const uint8_t EPD_DC   = 17; // to EPD DC
 static const uint8_t EPD_SCK  = 18; // to EPD CLK
