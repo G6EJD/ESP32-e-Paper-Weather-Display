dbb28452eb88e1791c144bbf6c7a376ffd418a4e
G6EJD <d.l.bird@sky.com>
2020-11-06 18:48:30 +0000

    Update corrects forecast Timezone offset, was in UTC

--- examples/Waveshare_9_7/Waveshare_9_7.ino
+++ examples/Waveshare_9_7/Waveshare_9_7.ino
@@ -359,7 +359,7 @@ void DisplayForecastWeather(int x, int y, int index) {
   display.drawLine(x, y + 40, x + fwidth - 3, y + 40, GxEPD_BLACK);
   DisplayConditionsSection(x + fwidth / 2, y + 90, WxForecast[index].Icon, SmallIcon);
   u8g2Fonts.setFont(u8g2_font_helvB14_tf);
-  drawString(x + fwidth / 2 - 10, y + 20, String(WxForecast[index].Period.substring(11, 16)), CENTER);
+  drawString(x + fwidth / 2 - 10, y + 20, String(ConvertUnixTime(WxForecast[index].Dt + WxConditions[0].timezone).substring(0,5)), CENTER);
   drawString(x + fwidth / 2 + 0, y + 130, String(WxForecast[index].High, 0) + "°/" + String(WxForecast[index].Low, 0) + "°", CENTER);
 }
 //#########################################################################################
@@ -678,6 +678,38 @@ boolean UpdateLocalTime() {
   Date_str = day_output;
   Time_str = time_output;
   return true;
+}
+//#########################################################################################
+String ConvertUnixTime(int unix_time) {
+  struct tm *now_tm;
+  int hour, min, second, day, month, year, wday;
+  // timeval tv = {unix_time,0};
+  time_t tm = unix_time;
+  now_tm = localtime(&tm);
+  hour   = now_tm->tm_hour;
+  min    = now_tm->tm_min;
+  second = now_tm->tm_sec;
+  wday   = now_tm->tm_wday;
+  day    = now_tm->tm_mday;
+  month  = now_tm->tm_mon + 1;
+  year   = 1900 + now_tm->tm_year; // To get just YY information
+  MoonDay   = day;
+  MoonMonth = month;
+  MoonYear  = year;
+  if (Units == "M") {
+    time_str =  (hour < 10 ? "0" + String(hour) : String(hour)) + ":" + (min < 10 ? "0" + String(min) : String(min)) + ":" + "  ";                     // HH:MM   05/07/17
+    time_str += (day < 10 ? "0" + String(day) : String(day)) + "/" + (month < 10 ? "0" + String(month) : String(month)) + "/" + (year < 10 ? "0" + String(year) : String(year)); // HH:MM   05/07/17
+  }
+  else {
+    String ampm = "am";
+    if (hour > 11) ampm = "pm";
+    hour = hour % 12; if (hour == 0) hour = 12;
+    time_str =  (hour % 12 < 10 ? "0" + String(hour % 12) : String(hour % 12)) + ":" + (min < 10 ? "0" + String(min) : String(min)) + ampm + " ";      // HH:MMam 07/05/17
+    time_str += (month < 10 ? "0" + String(month) : String(month)) + "/" + (day < 10 ? "0" + String(day) : String(day)) + "/" + "/" + (year < 10 ? "0" + String(year) : String(year)); // HH:MMpm 07/05/17
+  }
+  // Returns either '21:12  ' or ' 09:12pm' depending on Units
+  //Serial.println(time_str);
+  return time_str;
 }
 //#########################################################################################
 void DrawBattery(int x, int y) {
