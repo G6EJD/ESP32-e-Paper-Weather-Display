35ea9aaef568b15f5a34b00b8970004bd1492a38
G6EJD <d.l.bird@sky.com>
2020-11-10 10:48:49 +0000

    More UTC updates for Forecast

--- examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
+++ examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
@@ -561,38 +561,6 @@ boolean UpdateLocalTime() {
   return true;
 }
 //#########################################################################################
-String ConvertUnixTime(int unix_time) {
-  struct tm *now_tm;
-  int hour, min, second, day, month, year, wday;
-  // timeval tv = {unix_time,0};
-  time_t tm = unix_time;
-  now_tm = localtime(&tm);
-  hour   = now_tm->tm_hour;
-  min    = now_tm->tm_min;
-  second = now_tm->tm_sec;
-  wday   = now_tm->tm_wday;
-  day    = now_tm->tm_mday;
-  month  = now_tm->tm_mon + 1;
-  year   = 1900 + now_tm->tm_year; // To get just YY information
-  MoonDay   = day;
-  MoonMonth = month;
-  MoonYear  = year;
-  if (Units == "M") {
-    time_str =  (hour < 10 ? "0" + String(hour) : String(hour)) + ":" + (min < 10 ? "0" + String(min) : String(min)) + ":" + "  ";                     // HH:MM   05/07/17
-    time_str += (day < 10 ? "0" + String(day) : String(day)) + "/" + (month < 10 ? "0" + String(month) : String(month)) + "/" + (year < 10 ? "0" + String(year) : String(year)); // HH:MM   05/07/17
-  }
-  else {
-    String ampm = "am";
-    if (hour > 11) ampm = "pm";
-    hour = hour % 12; if (hour == 0) hour = 12;
-    time_str =  (hour % 12 < 10 ? "0" + String(hour % 12) : String(hour % 12)) + ":" + (min < 10 ? "0" + String(min) : String(min)) + ampm + " ";      // HH:MMam 07/05/17
-    time_str += (month < 10 ? "0" + String(month) : String(month)) + "/" + (day < 10 ? "0" + String(day) : String(day)) + "/" + "/" + (year < 10 ? "0" + String(year) : String(year)); // HH:MMpm 07/05/17
-  }
-  // Returns either '21:12  ' or ' 09:12pm' depending on Units
-  //Serial.println(time_str);
-  return time_str;
-}
-//#########################################################################################
 void DrawBattery(int x, int y) {
   uint8_t percentage = 100;
   float voltage = analogRead(35) / 4096.0 * 7.46;
