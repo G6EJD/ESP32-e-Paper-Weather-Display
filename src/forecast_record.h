da0a960a5559fb95d5eb8920a14456f1c673ce8d
G6EJD <d.l.bird@sky.com>
2021-02-03 09:52:45 +0000

    Added 'FeelsLike' to the forecast record

--- src/forecast_record.h
+++ src/forecast_record.h
@@ -18,7 +18,7 @@ typedef struct { // For current Day and Day 1, 2, 3, etc
   float    lat;
   float    lon;
   float    Temperature;
-  float    feels_like;
+  float    Feelslike;
   float    Humidity;
   float    High;
   float    Low;
