231071dbf36fced2517820224faee2ae7db220ba
G6EJD <d.l.bird@sky.com>
2021-03-07 13:03:46 +0000

    Update Waveshare_7_5_T7.ino

--- examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
+++ examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
@@ -952,6 +952,7 @@ void drawStringMaxWidth(int x, int y, unsigned int text_width, String text, alig
 //#########################################################################################
 void InitialiseDisplay() {
   display.init(115200, true, 2, false); // init(uint32_t serial_diag_bitrate, bool initial, uint16_t reset_duration, bool pulldown_rst_mode)
+  // display.init(); for older Waveshare HAT's
   SPI.end();
   SPI.begin(EPD_SCK, EPD_MISO, EPD_MOSI, EPD_CS);
   u8g2Fonts.begin(display); // connect u8g2 procedures to Adafruit GFX
