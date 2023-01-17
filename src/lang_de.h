5e38097b068230a2cb9315656082618c55318151
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-01-17 18:12:10 +0100

    Add files via upload

---
+++ src/lang_de.h
@@ -0,0 +1,82 @@
+#define FONT(x) x##_tf
+
+//Temperatur - Luftfeuchtigkeit - Vorschau
+const String TXT_FORECAST_VALUES      = "3-Tages-Vorhersage";
+const String TXT_LOCAL_HISTORY_VALUES = "Verlauf";
+const String TXT_MQTT_HISTORY_VALUES  = "Verlauf";
+const String TXT_CONDITIONS           = "Aktuelles Wetter";
+const String TXT_DAYS                 = "(Tage)";
+const String TXT_HOURS                = "(Stunden)";
+const String TXT_TEMPERATURES         = "Temperatur";
+const String TXT_TEMPERATURE_C        = "Temperatur [°C]";
+const String TXT_TEMPERATURE_F        = "Temperatur [°F]";
+const String TXT_HUMIDITY_PERCENT     = "Luftfeuchte [%]";
+
+//Luftdruck
+const String TXT_PRESSURE         = "Luftdruck";
+const String TXT_PRESSURE_HPA     = "Luftdruck [hPa]";
+const String TXT_PRESSURE_IN      = "Luftdruck [in]";
+const String TXT_PRESSURE_STEADY  = "beständig";
+const String TXT_PRESSURE_RISING  = "steigend";
+const String TXT_PRESSURE_FALLING = "fallend";
+
+//Niederschlag / Schneefall
+const String TXT_RAINFALL_MM = "Regen [mm]";
+const String TXT_RAINFALL_IN = "Regen [in]";
+const String TXT_SNOWFALL_MM = "Schnee [mm]";
+const String TXT_SNOWFALL_IN = "Schnee [in]";
+const String TXT_PRECIPITATION_SOON = "in Kürze";
+
+//Sonne
+const String TXT_SUNRISE  = "Sonnenaufg.";
+const String TXT_SUNSET   = "Sonnenunterg.";
+
+//Mond
+const String TXT_MOON_NEW             = "Neumond";              // Neumond
+const String TXT_MOON_WAXING_CRESCENT = "zunehmende  Sichel";   // erstes Viertel bzw. zunehmende Sichel
+const String TXT_MOON_FIRST_QUARTER   = "zunehmender Halbmond"; // zunehmender Halbmond 
+const String TXT_MOON_WAXING_GIBBOUS  = "zweites Viertel";      // zweites Viertel
+const String TXT_MOON_FULL            = "Vollmond";             // Vollmond
+const String TXT_MOON_WANING_GIBBOUS  = "drittes Viertel";      // drittes Viertel 
+const String TXT_MOON_THIRD_QUARTER   = "abnehmender Halbmond"; // abnehmender Halbmond
+const String TXT_MOON_WANING_CRESCENT = "abnehmende Sichel";    // letztes Viertel bzw. abnehmende Sichel 
+
+//Energie / WLAN
+const String TXT_POWER    = "Akku";
+const String TXT_WIFI     = "WLAN";
+const String TXT_WIFI_OFF = "WLAN nicht verbunden";
+const char* TXT_UPDATED   = "aktualisiert:";
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "Windgeschw./Richtung";
+const String TXT_N   = "N";
+const String TXT_NNE = "NNO";
+const String TXT_NE  = "NO";
+const String TXT_ENE = "ONO";
+const String TXT_E   = "O";
+const String TXT_ESE = "OSO";
+const String TXT_SE  = "SO";
+const String TXT_SSE = "SSO";
+const String TXT_S   = "S";
+const String TXT_SSW = "SSW";
+const String TXT_SW  = "SW";
+const String TXT_WSW = "WSW";
+const String TXT_W   = "W";
+const String TXT_WNW = "WNW";
+const String TXT_NW  = "NW";
+const String TXT_NNW = "NNW";
+
+//Wochentag
+/*
+const char* weekday_D[] = { "Sonntag",
+                             "Montag",   
+                             "Dienstag", 
+                             "Mittwoch", 
+                             "Donnerstag", 
+                             "Freitag", 
+                             "Samstag"};
+*/
+const char* weekday_D[] =  { "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" };
+
+//Monat
+const char* month_M[] = { "Jan", "Feb", "März", "Apr", "Mai", "Juni", "Juli", "Aug", "Sep", "Okt", "Nov", "Dez" };
