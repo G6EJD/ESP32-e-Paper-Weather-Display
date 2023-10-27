a9486b8f8691e40c8b03bedb5ff18ffe2d9dfdd9
jpolton <jpolton@gmail.com>
2023-10-27 23:04:44 +0100

    My pin layout and preferred sleep setting times

--- examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
+++ examples/Waveshare_7_5_T7/Waveshare_7_5_T7.ino
@@ -43,13 +43,13 @@
 enum alignment {LEFT, RIGHT, CENTER};
 
 // Connections for e.g. LOLIN D32
-static const uint8_t EPD_BUSY = 4;  // to EPD BUSY
-static const uint8_t EPD_CS   = 5;  // to EPD CS
-static const uint8_t EPD_RST  = 16; // to EPD RST
-static const uint8_t EPD_DC   = 17; // to EPD DC
-static const uint8_t EPD_SCK  = 18; // to EPD CLK
-static const uint8_t EPD_MISO = 19; // Master-In Slave-Out not used, as no data from display
-static const uint8_t EPD_MOSI = 23; // to EPD DIN
+//static const uint8_t EPD_BUSY = 4;  // to EPD BUSY
+//static const uint8_t EPD_CS   = 5;  // to EPD CS
+//static const uint8_t EPD_RST  = 16; // to EPD RST
+//static const uint8_t EPD_DC   = 17; // to EPD DC
+//static const uint8_t EPD_SCK  = 18; // to EPD CLK
+//static const uint8_t EPD_MISO = 19; // Master-In Slave-Out not used, as no data from display
+//static const uint8_t EPD_MOSI = 23; // to EPD DIN
 
 // Connections for e.g. Waveshare ESP32 e-Paper Driver Board
 static const uint8_t EPD_BUSY = 25;
