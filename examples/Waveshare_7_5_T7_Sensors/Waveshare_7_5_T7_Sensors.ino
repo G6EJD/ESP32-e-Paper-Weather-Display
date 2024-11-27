d1d0ffd1e531d437680b8a4f0b238499bdd295d1
Matthias Prinke <m.prinke@arcor.de>
2024-11-27 22:22:39 +0100

    Moved #include "bitmap_local/_remote.h" to config.h

--- examples/Waveshare_7_5_T7_Sensors/Waveshare_7_5_T7_Sensors.ino
+++ examples/Waveshare_7_5_T7_Sensors/Waveshare_7_5_T7_Sensors.ino
@@ -76,8 +76,6 @@
 #include "WeatherSymbols.h"        // Functions for drawing weather symbols at runtime
 #include "bitmap_icons.h"          // Icon bitmaps
 #include "bitmap_weather_report.h" // Picture shown on ScreenStart
-#include "bitmap_local.h"          // Picture shown on ScreenLocal - replace by your own
-#include "bitmap_remote.h"         // Picture shown on ScreenMQTT  - replace by your own
 #include "utils.h"
 #include "secrets.h"
 
