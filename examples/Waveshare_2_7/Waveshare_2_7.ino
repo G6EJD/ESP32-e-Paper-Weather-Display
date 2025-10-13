6a9a37d6cb9e9f8212fe6f1f236f522332e8e95f
G6EJD - David <d.l.bird@sky.com>
2025-10-13 16:30:10 +0100

    Update Waveshare_2_7.ino

--- examples/Waveshare_2_7/Waveshare_2_7.ino
+++ examples/Waveshare_2_7/Waveshare_2_7.ino
@@ -25,9 +25,7 @@
 #include <SPI.h>
 #define  ENABLE_GxEPD2_display 0
 #include <GxEPD2_BW.h>
-#include <GxEPD2_3C.h>
 #include <U8g2_for_Adafruit_GFX.h>
-#include "forecast_record.h"
 //#include "lang.h"                   // Localisation (English)
 //#include "lang_cz.h"                // Localisation (Czech)
 //#include "lang_fr.h"                // Localisation (French)
@@ -123,14 +121,13 @@ void setup() {
     //if ((CurrentHour >= WakeupTime && CurrentHour <= SleepTime)) {
       InitialiseDisplay(); // Give screen time to initialise by getting weather data!
       byte Attempts = 1;
-      bool RxWeather = false, RxForecast = false;
+      bool RxWeather = false;
       WiFiClient client;   // wifi client object
-      while ((RxWeather == false || RxForecast == false) && Attempts <= 5) { // Try up-to 2 time for Weather and Forecast data
-        if (RxWeather  == false) RxWeather  = obtain_wx_data(client, "weather");
-        if (RxForecast == false) RxForecast = obtain_wx_data(client, "forecast");
+      while (RxWeather == false && Attempts <= 5) { // Try up-to 2 time for Weather and Forecast data
+        if (RxWeather  == false) RxWeather  = ReceiveOneCallWeather(client, true);
         Attempts++;
       }
-      if (RxWeather && RxForecast) { // Only if received both Weather or Forecast proceed
+      if (RxWeather) { // Only if received both Weather or Forecast proceed
         StopWiFi(); // Reduces power consumption
         DisplayWeather();
         display.display(false); // Full screen update mode
